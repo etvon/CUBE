cf2844e7c56edb5dc409edc6abae1f5e344e3b8f
etvon <etvon@live.com>
2024-03-06 15:49:48 +0800

    Initial Github Pages deployment

---
+++ index.search.js
@@ -0,0 +1,90 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "eWeLink CUBE",
+    "content": "Check the Release Notes or Visit the eWeLink Forum to learn what’s new in recent updates of eWeLink CUBE.\nOfficial Release Notes\neWeLink Forum\n",
+    "description": "",
+    "tags": null,
+    "title": "What's NEW",
+    "uri": "/1-whats-new/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE",
+    "content": "Here are some documents or videos about eWeLink CUBE powered products.\nSONOFF iHost User Guides\neWeLink Youtube Channel\n",
+    "description": "",
+    "tags": null,
+    "title": "User Guide",
+    "uri": "/user-guide/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE",
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Supported Device",
+    "uri": "/supported-device/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE",
+    "content": "What is an Add-on? It is any third-party software program or script that is added to a program to give it additional features and abilities.\nAdd-ons in eWeLink CUBE Add-ons allow the user to extend the functionality around eWeLink CUBE by installing additional applications. This can be running an application that eWeLink CUBE can integrate with (like Node-red). Add-ons can be added via the Docker menu in eWeLink CUBE.\n",
+    "description": "",
+    "tags": null,
+    "title": "Add-Ons",
+    "uri": "/add-on/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "eWeLink CUBE",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE \u003e Add-Ons",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "eWeLink Smart Home",
+    "uri": "/add-on/ewelink-smart-home/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE \u003e Add-Ons",
+    "content": "Install and Run Go to the Docker menu and install Node-red, if it is not there, click the add button to add. Click the RUN button to run the Node-red, keep parameters default. Open the webUI of Node-red After running, click the ‘More info’ to go to its detail page. Click the ‘Show in new window’ button upper right to open its web console on a new tab. Install the ewelink-cube palette On the Node-red page, click the menu upper right, click ‘Manage palette’. Switch to ‘Install’ tab and search for ’ewelink-cube’, install the ’node-red-contrib-ewelink-cube’. Get iHost access token Drag and drop the ‘get-devices’ node and ‘debug’ node, connect them. Double click on the ‘get-devices’ to edit. Click the edit button to add a new server. Enter your iHost IP and click the search button, then click ‘GET TOKEN’ Go back to iHost page and Click Allow. Click Add and select All device, save the settings. Add an ‘inject’ node and click the Deploy button upper right. Click on the inject node and you’ll see the output under debug tab. For other nodes, you can switch to the info tab to check the description. ",
+    "description": "",
+    "tags": null,
+    "title": "Node-red",
+    "uri": "/add-on/nodered/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE \u003e What's NEW",
+    "content": "V1.13.3 (2024.1.31) Optimizations:\nOptimized the display of SNZB-06P detection time interval. Bug Fixes:\nFixed an issue where the volume list was empty in some cases. Fixed an issue where Tailscale’s UI could not be displayed in some cases. V1.13.2 (2024.1.23) New Features:\nCustom language is available. Now your iHost can display in the language you want by uploading language files, check it under Settings menu. Optimizations:\nOptimized the UI of the Smart Security page. Optimized the firmware update process. Added ‘About’ menu to installed add-ons page. Bug Fixes:\nFixed the issue where add-ons couldn’t load the Web UI. Fixed the bug that Matter Bridge could not initialize after restarting iHost in muted state. Fixed an issue where the names of devices synchronized via Matter Bridge changed after iHost was restarted. V1.13.0 (2023.12.26) New Features:\nNow you can drag and drop the scence actions to rearrange their order. Support for adding SNZB-03P Motion Sensor, SNZB-04P Door/Window Sensor, and NSPanelP-Router. Paral-sync add-on now supports SNZB-03P Motion Sensor, SNZB-04P Door/Window Sensor, SNZB-02D Temp.\u0026Humi. Sensor, and TRVZB Thermostatic Remote Valve (please update Paral-sync add-on to the latest version on its detail page). eWeLink Smart Home add-on now supports cloud control of some devices and LAN control of more sensor and light sub-devices of SONOFF Zigbee Bridge Pro. (please update eWeLink Smart Home add-on to the latest version on its detail page). Optimizations:\nIncreased the limit of CAST dashboards to 10. Optimized logic for adding, deleting, and synchronizing RF remote controllers in eWeLink Smart Home add-on. Sidebar UI design optimized. Added an entry to open add-on page in a new window. Bug Fixes:\nResolved the issue where the same device couldn’t repeatedly be used as scene actions in a scene. Fixed the bug where Temp.\u0026Humi. sensors couldn’t save 1 decimal place as scene triggers. Fixed the occasional pairing issue with Matter Bridge. Resolved the problem where device names changed in Matter-supported platforms after iHost was restarted. ",
+    "description": "",
+    "tags": null,
+    "title": "Official Release Note",
+    "uri": "/1-whats-new/official-release-notes/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE \u003e What's NEW",
+    "content": "Welcome to the official eWeLink Forum, a vibrant community where users can discuss, share insights, and seek support for all things related to eWeLink products, including the innovative eWeLink CUBE.\neWeLink Forum\neWeLink Forum - CUBE catagory\nWe encourage you to participate actively in the discussions, ask questions, and share your experiences with the eWeLink CUBE and other eWeLink products. Our knowledgeable community members are always eager to assist and provide valuable feedback.\n",
+    "description": "",
+    "tags": null,
+    "title": "eWeLink Forum",
+    "uri": "/1-whats-new/forum/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "eWeLink CUBE",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
